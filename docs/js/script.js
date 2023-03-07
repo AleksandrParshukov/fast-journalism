45fc2d44d16ae1c0a40440da13e46c8e698a1213
Aleksandr Parshukov <porshe97@yandex.ru>
2023-03-07 17:06:00 +0300

    main

---
+++ docs/js/script.js
@@ -0,0 +1,128 @@
+$('document').ready(function () {
+  const $leaders_list = $('#leaders_list'),
+    $participants_list = $('#participants_list');
+
+  const $leaders_item = $(
+    '<li class="leaders__item d-flex">' +
+      '<p class="leaders__name">' +
+      '</p>' +
+      '<p class="leaders__scores"></p>' +
+      '</li>'
+  );
+
+  const $participants_item = $(
+    '<div class="participants__item d-flex">' +
+      '<p class="participants__name"></p>' +
+      '<p class="participants__scores"></p>' +
+      '</div>'
+  );
+
+  $.ajax({
+    url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS74DnCragK24SidpUHoaMbpJ4InMtazJOwu61xizQUx71q54YSjzmls54dJLqhil-5xBm2T-D5gbyE/pub?output=csv',
+    dataType: 'text',
+    cache: false,
+    success: function (data) {
+      var items = $.csv.toObjects(data);
+
+      console.log(items);
+
+      items.sort(function (x, y) {
+        return y['ИТОГ'] - x['ИТОГ'];
+      });
+
+      $.each(items, function (i, val) {
+        if (i < 3) {
+          add_leaders_item(val);
+        } else {
+          add_participants_item(val);
+        }
+      });
+
+      /* $participants_list.slick({
+        vertical: true,
+        verticalSwiping: false,
+        slidesToShow: 9,
+        autoplay: true,
+        accessibility: false,
+        arrows: false,
+        autoplaySpeed: 1500,
+        speed: 500,
+        waitForAnimate: false,
+        infinite: true,
+        pauseOnFocus: false,
+      }); */
+
+      let $wrapper = $participants_list,
+        $marquees = $wrapper.find('.participants__item'),
+        wrapperOffset = $wrapper.offset().top;
+
+      clearInterval(interval);
+
+      function move() {
+        $.each($marquees, function (i) {
+          let $marquee = $(this),
+            currentTY = $marquee.css('transform').split(','),
+            marqueeHeight = $marquee.outerHeight(true),
+            offset = $marquee.offset().top + marqueeHeight;
+
+          if (currentTY[5] === undefined) {
+            currentTY = -1;
+          } else {
+            currentTY = parseFloat(currentTY[5]) - 1;
+          }
+
+          $marquee.css('transform', 'translateY(' + currentTY + 'px)');
+          /*   if (i == 0) {
+            console.log(i + ': ' + wrapperOffset);
+            console.log(i + ': ' + offset);
+            console.log(offset <= wrapperOffset);
+          }
+ */
+          if (offset <= wrapperOffset) {
+            $wrapper.append($marquee);
+            update();
+          }
+        });
+
+        function update() {
+          $.each($marquees, function () {
+            let $marquee = $(this),
+              currentTY = $marquee.css('transform').split(','),
+              marqueeHeight = $marquee.outerHeight(true);
+
+            if (currentTY[5] === undefined) {
+              currentTY = 0;
+            } else {
+              currentTY = parseFloat(currentTY[5]);
+            }
+
+            $marquee.css(
+              'transform',
+              'translateY(' + (currentTY + marqueeHeight) + 'px)'
+            );
+          });
+        }
+      }
+
+      var interval = setInterval(move, 20);
+    },
+  });
+
+  function add_leaders_item(data) {
+    let $new_item = $leaders_item.clone();
+
+    $new_item.find('.leaders__name').text(data['Название команды']);
+    $new_item.find('.leaders__scores').text(data['ИТОГ']);
+
+    $leaders_list.append($new_item);
+  }
+
+  function add_participants_item(data) {
+    let $new_item = $participants_item.clone();
+
+    $new_item.find('.participants__name').text(data['Название команды']);
+    $new_item.find('.participants__scores').text(data['ИТОГ']);
+
+    $participants_list.append($new_item);
+  }
+});
---
+++ source/js/script.js
@@ -0,0 +1,72 @@
+$('document').ready(function () {
+	let place = 1;
+	const $leaders_list = $('#leaders_list');
+
+	const $leaders_item = $(
+		'<li class="leaders__item d-flex">' +
+			'<p class="leaders__place"></p>' +
+			'<p class="leaders__name"></p>' +
+			'<p class="leaders__scores"></p>' +
+			'</li>'
+	);
+
+	const $participants_item = $(
+		'<li class="participants__item d-flex">' +
+			'<p class="participants__place"></p>' +
+			'<p class="participants__name"></p>' +
+			'<p class="participants__scores"></p>' +
+			'</li>'
+	);
+
+	$.ajax({
+		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSdP3ccei1KvSVk2faTVTEmJgRXd7V6KaU0-Wa3qiV1SsqqGcnBuu7A0H_JBIcU9K6sgQ8rZP_yDoG_/pub?output=csv',
+		dataType: 'text',
+		cache: false,
+		success: function (data) {
+			var items = $.csv.toObjects(data);
+
+			console.log(items);
+
+			items.sort(function (x, y) {
+				return to_float(y['ИТОГ']) - to_float(x['ИТОГ']);
+			});
+
+			$.each(items, function (i, val) {
+				if (i < 3) {
+					add_leaders_item(val);
+				} else if (i < 9) {
+					add_participants_item(val, '1');
+				} else if (i < 15) {
+					add_participants_item(val, '2');
+				} else {
+					add_participants_item(val, '3');
+				}
+			});
+		},
+	});
+
+	function add_leaders_item(data) {
+		let $new_item = $leaders_item.clone();
+
+		$new_item.find('.leaders__place').text(place++);
+		$new_item.find('.leaders__name').text(data['Название команды']);
+		$new_item.find('.leaders__scores').text(data['ИТОГ']);
+
+		$leaders_list.append($new_item);
+	}
+
+	function add_participants_item(data, num) {
+		const $participants_list = $('#participants_list' + num),
+			$new_item = $participants_item.clone();
+
+		$new_item.find('.participants__place').text(place++);
+		$new_item.find('.participants__name').text(data['Название команды']);
+		$new_item.find('.participants__scores').text(data['ИТОГ']);
+
+		$participants_list.append($new_item);
+	}
+
+	function to_float(num) {
+		return parseFloat(num.replace(/,/, '.'));
+	}
+});
